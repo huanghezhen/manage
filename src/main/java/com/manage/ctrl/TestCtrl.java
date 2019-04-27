7c02d913683d8910145101811bd35de0683e8a7a
huanghz <huanghz@51kezhi.com>
2019-04-27 12:49:57 +0800

    添加序列

---
+++ src/main/java/com/manage/ctrl/TestCtrl.java
@@ -0,0 +1,26 @@
+package com.manage.ctrl;
+
+import com.manage.entity.Ret;
+import com.manage.util.SequenceGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName: TestCtrl
+ * @Description: TODO
+ * @author: huanghz
+ * @date: 2019/4/27 12:21
+ */
+@RestController
+@RequestMapping("/testCtrl")
+public class TestCtrl
+{
+    @Autowired
+    private SequenceGenerator sequenceGenerator;
+
+    @RequestMapping("/getSeq")
+    public Ret getSeq(){
+        return Ret.getRet(sequenceGenerator.next());
+    }
+}
---
+++ src/main/java/com/manage/util/SequenceGenerator.java
@@ -0,0 +1,41 @@
+package com.manage.util;
+
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @ClassName: SequenceGenerator
+ * @Description: TODO
+ * @author: huanghz
+ * @date: 2019/4/27 11:53
+ */
+@Component
+public class SequenceGenerator
+{
+    private long             min;
+    private long             max;
+    private long             counter;
+    private SimpleDateFormat format17;
+
+    public SequenceGenerator()
+    {
+        min = 10000L;
+        max = 99999L;
+        counter = 10000L;
+        format17 = new SimpleDateFormat("yyyyMMddHHmmssSSS");
+    }
+
+    // 获取序列号
+    public synchronized String next()
+    {
+        String dateStr = format17.format(new Date());
+        if (counter > max)
+        {
+            counter = min;
+        }
+        return dateStr + (counter++);
+    }
+
+}

eb25316b3fe6be82f6b91b8fb8598ea67e8d2525
Z-cl <zhang_cl_cn@163.com>
2019-05-06 15:31:20 +0800

    默认页面的设置

---
+++ src/main/java/com/manage/config/DefaultView.java
@@ -0,0 +1,24 @@
+package com.manage.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @ClassName: DefaultView
+ * @Description: TODO
+ * @author: cl-Z
+ * @date: 2019/5/6 15:17
+ * @Version: V1.0
+ */
+@Configuration
+public class DefaultView extends WebMvcConfigurerAdapter {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("/view/user/login.html");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        super.addViewControllers(registry);
+
+    }
+}

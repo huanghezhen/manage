863ce0d35580c6854c0a7dd02a7ec24d1167d072
Z-cl <zhang_cl_cn@163.com>
2019-05-08 10:18:55 +0800

    新增常量以及结果返回数据

--- src/main/java/com/manage/config/Const.java
+++ src/main/java/com/manage/config/Const.java
@@ -17,6 +17,8 @@ public class Const
     public static final String SUCCEED_MSG = "成功";
     public static final String SESSION_USER = "SESSION_USER";
     public static final String SESSION_CODE = "SESSION_CODE";
+    public static final Integer PRODUCT_UP_STATUS=1;
+    public static final Integer PRODUCT_DOWN_STATUS=0;
 
     public enum failedEnum{
         SAVE_MERCHANT_FAILED("-1", "注册商户失败"),
@@ -24,7 +26,13 @@ public class Const
         SESSION_CODE_FAILED("-3", "验证码错误"),
         UNUSER("-4", "用户不存在"),
         UNPASSWD("-5", "密码错误"),
-        UNLOGIN("-6", "用户未登陆")
+        UNLOGIN("-6", "用户未登陆"),
+        PRODUCT_SAVE_ERROR("-7","商品增加失败"),
+        PRODUCT_NOT_EXIT("-8","商品不存在"),
+        PRODUCT_CATEGORY_NOT_EXIT("-9","此商品分类不存在"),
+        PRODUCT_CATEGORY_SON_NOT_EXIT("-10","此商品分类不存在"),
+        SAVE_ERROR("-11","新增失败"),
+        UPDATE_ERROR("-12","修改失败")
         ;
 
         // 错误码

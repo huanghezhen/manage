02acf773a0bc42d717d1c989f6ba8cd6d1e0881c
Z-cl <zhang_cl_cn@163.com>
2019-05-08 10:39:55 +0800

    商品分类Mapper

---
+++ src/main/java/com/manage/dao/productCategory/ProductCategoryMapper.java
@@ -0,0 +1,24 @@
+package com.manage.dao.productCategory;
+
+import com.manage.entity.productCategory.ProductCategoryModel;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * @ClassName: ProductCategoryMapper
+ * @Description: TODO
+ * @author: cl-Z
+ * @date: 2019/5/7 0007 17:01
+ * @Version: V1.0
+ */
+@Component("productCategoryMapper")
+public interface ProductCategoryMapper {
+    ProductCategoryModel findOne(int categoryId);
+    List<ProductCategoryModel> findAll();
+    int updateProductCategory(ProductCategoryModel productCategoryModel);
+    int saveProductCategory(ProductCategoryModel productCategoryModel);
+    List<ProductCategoryModel> findByParentId(int parentId);
+    List<ProductCategoryModel> findByIdIn(List<Integer> categoryIds);
+
+}

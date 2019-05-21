0c3ba376d63431cd812abbd5d62c4fbdaa77655e
Z-cl <zhang_cl_cn@163.com>
2019-05-21 17:18:31 +0800

    LayUI模型类的添加

---
+++ src/main/java/com/manage/vo/LayEditVo.java
@@ -0,0 +1,30 @@
+package com.manage.vo;
+
+/**
+ * @ClassName: LayEditVo
+ * @Description: TODO
+ * @author: cl-Z
+ * @date: 2019/5/21 0021 15:32
+ * @Version: V1.0
+ */
+public class LayEditVo {
+
+    private String src;
+    private String title;
+
+    public String getSrc() {
+        return src;
+    }
+
+    public void setSrc(String src) {
+        this.src = src;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
---
+++ src/main/java/com/manage/vo/TableVo.java
@@ -0,0 +1,12 @@
+package com.manage.vo;
+
+/**
+ * @ClassName: TableVo  表格的视图模型
+ * @Description: TODO
+ * @author: cl-Z
+ * @date: 2019/5/20 0020 9:46
+ * @Version: V1.0
+ */
+public class TableVo {
+
+}
